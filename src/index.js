d18c0687a5dbfa52ecaa7292b675824cfb42556a
thewebmasterp <thewebmsaterp@gmail.com>
2021-11-06 09:39:28 +0200

    Now, that's the actual code. Below that version was the old one that had nothing to do with this package

--- src/index.js
+++ src/index.js
@@ -6,62 +6,71 @@ import systeminfo from "systeminformation";
 export default class Text extends EventEmitter {
   constructor(options, output) {
     super();
-    options = options || {};
-    this.output = output || {};
-    this.chargingColor = options.chargingColor || "#666666";
-    this.dischargingColor = options.dischargingColor || "#AAAA00";
-    this.criticalThreshold = options.criticalThreshold || 30;
+    options = options || {}
+    this.output = output || {}
+    this.chargingColor = options.chargingColor || '#a6cc33'
+    this.chargingChar = options.chargingChar || '🔌'
+    this.dischargingColor = options.dischargingColor || '#c38418'
+    this.dischargingChar = options.dischargingChar || '🔋'
+    this.criticalChargeChar = options.criticalChargeChar || '⚠️' 
+    this.criticalThreshold = options.criticalThreshold || 30
   }
 
   update() {
     systeminfo
       .battery(info => {
-        let msg = info.percent + "%";
-        if (!info.ischarging && info.percent !== 100) {
-          if (info.timeremaining > 0) {
-            msg += " (" + this.formatMinutes(info.timeremaining) + ")";
-          }
-          if (info.timeremaining < this.criticalThreshold) {
-            this.output.critical = true;
+        let char = this.chargingChar
+        let msg = `${info.percent}%`
+        if (!info.ischarging) { //is discharging
+          char = this.dischargingChar
+          if (info.percent !== 100) {
+            if (info.timeremaining > 0) {
+                msg += ` (${this.formatMinutes(info.timeremaining)})`
+            }
+            if (info.timeremaining < this.criticalThreshold) {
+                this.output.critical = true
+                char = this.criticalChargeChar
+            }
           }
         }
+        msg = `${char} ${msg}`
 
-        this.output.full_text = msg;
-        this.output.short_text = msg;
+        this.output.full_text = msg
+        this.output.short_text = msg
 
         // Color indicating loading status
         if (info.ischarging === true) {
-          this.changeColor(this.chargingColor);
+          this.changeColor(this.chargingColor)
         } else {
-          this.changeColor(this.dischargingColor);
+          this.changeColor(this.dischargingColor)
         }
       })
       .catch(err => {
-        this.output.critical = true;
-        this.output.full_text = err;
-        this.output.short_text = err;
+        this.output.critical = true
+        this.output.full_text = err
+        this.output.short_text = err
       })
       .finally(msg => {
-        this.emit("updated", this, this.output);
-      });
+        this.emit('updated', this, this.output)
+      })
   }
 
   changeColor(hex) {
     if (this.output.critical !== true) {
-      this.output.color = hex;
+      this.output.color = hex
     }
   }
 
   formatMinutes(min) {
-    let h = 0;
+    let h = 0
     if (min > 60) {
-      h = Math.floor(min / 60);
-      min = min % 60;
+      h = Math.floor(min / 60)
+      min = min % 60
     }
-    return `${this.format(h)}:${this.format(min)}`;
-  }
+    return `${this.format(h)}:${this.format(min)}`
+  }1
 
   format(num) {
-    return num < 10 ? `0${num}` : num;
+    return num < 10 ? `0${num}` : num
   }
 }
